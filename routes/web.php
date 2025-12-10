8d0dfd6e2f26648b36f9fabd4da0d2a9fa65f277
Jimcl <jedllorente@gmail.com>
2025-12-11 00:25:24 +0800

    commit: learning laravel 11

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
+
+Route::get('/', [ExampleController::class, 'homepage']);
+
+Route::get('/about', [ExampleController::class, 'aboutPage']);
